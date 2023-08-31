97cd626250d953357ed1aadccecfc220de3dd20e
Wilson Silva <wilson.dsigns@gmail.com>
2023-08-31 21:22:34 +0700

    Refresh the .railsrc file and the templates
    What the HELL is Angular, Bower, PhantomJS 😂

---
+++ .rails/application_templates/api/main.rb
@@ -0,0 +1,32 @@
+def source_paths
+  [File.expand_path(File.dirname(__FILE__))]
+end
+
+gem 'dotenv-rails', require: 'dotenv/rails-now'
+
+gem_group :test do
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'guard-rubocop'
+  gem 'factory_bot_rails'
+  gem 'rspec-rails'
+  gem 'simplecov'
+  gem 'simplecov-console'
+  gem 'vcr'
+  gem 'webmock'
+end
+
+gem_group :development, :test do
+  gem 'brakeman', require: false
+  gem 'bundler-audit', require: false
+  gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'rubocop'
+end
+
+generate 'rspec:install'
+
+file '.rspec', <<-RSPEC
+--require spec_helper
+RSPEC
+
